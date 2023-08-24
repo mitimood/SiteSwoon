4d523e23d849980164b6e46633d0a88bb47b254c
SU0N <143121352+SU0N@users.noreply.github.com>
2023-08-24 15:20:30 -0300

    Update page.js

--- src/app/3d/page.js
+++ src/app/3d/page.js
@@ -4,8 +4,9 @@ import Link from 'next/link'
 export default function animations() {
   return (
     <main className="bg-[url('/bg.svg')] flex min-h-screen flex-col items-center justify-center p-10">
-      <iframe className='w-3/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/jLD0uf11LnI?si=WGKeP5Uijzp0LJ8E" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe className='w-3/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/40CleUOuShA?si=fETkCAynrQHaZnu9" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className='w-3/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/Prt41doYusE?si=uwX6SJyQrNfOdkxc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className='w-3/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/NGuFuSDcs0o?si=yQpIXAj416r8Si-l" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className='w-3/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/40CleUOuShA?si=_hYkotdoy64gQnYy" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </main>
   )
 }
