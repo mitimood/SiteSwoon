ff9b99543a985ff4d15a03946a2d50e6d1e635aa
mitimood <54547250+mitimood@users.noreply.github.com>
2023-08-28 01:00:14 -0300

    bug fix #2

--- src/app/art/page.js
+++ src/app/art/page.js
@@ -3,18 +3,18 @@ import Link from 'next/link'
 
 export default function animations() {
   return (
-    <main className="bg-[url('/bg.svg')] flex min-h-screen flex-col items-center justify-center p-10">
+    <main className="bg-[url('/chessy.svg')] flex min-h-screen flex-col items-center justify-center p-10">
         <Image src='/images/image02.jpg'     
         width="0"
     height="0"
     sizes="100vw"
-    className="w-2/6 h-auto mb-5 rounded-full"
+    className="w-full lg:w-3/6 h-auto mb-5 rounded-full"
     ></Image>
         <Image src='/images/F3JpzGqWMAERTyU.png'
         width="0"
     height="0"
     sizes="100vw"
-    className="w-2/6 h-auto mb-5"
+    className="w-full lg:w-3/6  h-auto mb-5"
   ></Image>
     </main>
   )
