03800074deb3bb2f7c4bae2fe36f956523d997da
SU0N <143121352+SU0N@users.noreply.github.com>
2024-04-29 08:56:35 -0300

    Update page.js

--- src/app/3d/page.js
+++ src/app/3d/page.js
@@ -4,7 +4,8 @@ import Link from 'next/link'
 export default function animations() {
   return (
     <main className="bg-[url('/chessy.svg')] flex min-h-screen flex-col items-center justify-center p-10">
-      <iframe className='w-6/6 lg:w-2/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/Prt41doYusE?si=uwX6SJyQrNfOdkxc"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" loading="lazy" allowfullscreen></iframe>
+<iframe className='w-6/6 lg:w-2/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/ijRcVGrfm9I?si=OD3QahrQLaCQjhLf"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" loading="lazy" allowfullscreen></iframe>      
+<iframe className='w-6/6 lg:w-2/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/Prt41doYusE?si=uwX6SJyQrNfOdkxc"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" loading="lazy" allowfullscreen></iframe>
       <iframe className='w-6/6 lg:w-2/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/NGuFuSDcs0o?si=yQpIXAj416r8Si-l"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" loading="lazy" allowfullscreen></iframe>
       <iframe className='w-6/6 lg:w-2/6 pb-10 rounded-xl aspect-video' src="https://www.youtube.com/embed/40CleUOuShA?si=_hYkotdoy64gQnYy"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" loading="lazy" allowfullscreen></iframe>
     </main>
